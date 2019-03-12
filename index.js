d47c1e09e92711b0f301bc541dafe805f9583bf1
Olivier Duhart <olivier.duhart@sage.com>
2019-03-12 14:57:30 +0100

    ics file generation

--- index.js
+++ index.js
@@ -7,20 +7,65 @@ var groupe_url = "http://t2t.29.fsgt.org/groupe/groupe-" + groupe
 
 var team = "PLABENNEC 4";
 
+var calFile = team.replace(" ", "").toLocaleLowerCase() + ".ics";
+
 
 matcheArray = Array();
 
 
+var matchLabel = function(match) {
+    if(match.local == team) {
+        return match.day.replace("\t","")+" "+match.remote.replace("\t","")+" (dom.)";
+    }
+    else {
+        return match.day.replace("\t","")+" "+match.local.replace("\t","")+" (ext.)";
+    }
+}
+
+var matchDate = function(match) {
+    parts = match.date.split("/");
+    date = new Date(parseInt(parts[2], 10),
+                  parseInt(parts[1], 10) - 1,
+                  parseInt(parts[0], 10));
+    
+    var dateStr = parts[2]+""+parts[1]+""+parts[0]+"T";
+
+    return dateStr;
+}
+
+var matchEventCal = function(match,cal) {
+    date = moment(match.date, 'MM/DD/YYYY');
+    lbl = matchLabel(match);
+    event = cal.createEvent({
+        start: date,
+        end: date.add(23, 'hour'),        
+        summary: matchLabel        
+    });
+}
+
+var matchEvent = function (match) {
+    fs.appendFile(calFile, "BEGIN:VEVENT\n");
+    date = matchDate(match);
+    fs.appendFile(calFile, "DTSTART:"+date+"203000Z\n");
+    fs.appendFile(calFile, "DTEND:"+date+"220000Z\n");
+    lbl = matchLabel(match);
+    fs.appendFile(calFile, "SUMMARY:"+lbl+"\n");
+    fs.appendFile(calFile, "DESCRIPTION:"+lbl+"\n");
+    fs.appendFile(calFile, "END:VEVENT\n");
+}
+
 var jsonToCal = function (matches) {
+
     fs.writeFile("./matches.json", "[");
+    fs.writeFile(calFile,"BEGIN:VCALENDAR\n");
+    fs.appendFile(calFile,"VERSION:2.0\n");
     for (l = 0; l < matches.length; l++) {
         m = matches[l];
-        if (m.local == team || m.remote == team) {
-            console.log(m);
-            fs.appendFile("./matches.json", JSON.stringify(m) + ",");
+        if (m.local == team || m.remote == team) {            
+            matchEvent(m);
         }
     }
-    fs.appendFile("./matches.json", "]");
+    fs.appendFile(calFile,"END:VCALENDAR\n");
 }
 
 
@@ -70,19 +115,11 @@ request.get(groupe_url, (error, response, html) => {
 
     jsonToCal(matchArray);
 
-    // fs.writeFile("./matches.json","[");
-    // for (l = 0; l < matcheArray.length; l++) {
-    //     m = matcheArray[l];
-    //     console.log(m);
-    //     fs.appendFile("./matches.json", JSON.stringify(m)+",");
-    // }
-    // fs.appendFile("./matches.json","]");
-
-
-})
+});
 
 
 
 request.get(groupe_url, process);
 
+ 
 
